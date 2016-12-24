9b4575e5f12d5485224ee4b6086c54b39bd2d431
dannycrews <dancrews@me.com>
2016-12-24 10:46:48 -0500

    Scaffold home_controller and add js to power search

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   mount Soulmate::Server, at: "/autocomplete"
 
   resources :verbs
   resources :nouns
 
-  # root 'home#index'
+  root 'home#index'
 
 end
